6be6ff04f56b5099ddba9b7ddfa8b508331e95ba
Emre YILMAZ <emreyilmaz@Kite.local>
2021-06-06 21:34:34 +0300

    asdasdasd

--- app/services/userStat.js
+++ app/services/userStat.js
@@ -17,24 +17,13 @@ const app = express.Router();
 
 //**************Route Level 1
 
-
-
-
-
 //get user book ex by id
-app.get(
-  "/:userId",
-  async (req, res) => {
-   
-
-
-    db.UStat.findAll({
-      where:{
-        userId :  req.params.userId,
-    
-      },
-     
-    })
+app.get("/:userId", async (req, res) => {
+  db.UStat.findAll({
+    where: {
+      userId: req.params.userId,
+    },
+  })
     .then((ex) => {
       return res.json({
         type: true,
@@ -47,10 +36,60 @@ app.get(
         data: e.toString(),
       });
     });
-  }
-  
-);
+});
 
+app.put("/stat-update", async (req, res) => {
+  const { amount, userId, name } = req.body;
 
+  db.UStat.findOne({
+    where: {
+      userId,
+      attainmentName: name.trim(),
+    },
+  })
+    .then((att) => {
+
+        if(att)
+        {
+
+            db.UStat.update(
+                {
+                  attainmentAmount: att.attainmentAmount + amount,
+                },
+                {
+                  where: {
+                    userId,
+                    attainmentName: name.trim(),
+                  },
+                }
+              )
+                .then((ret) => {
+                  return res.json({
+                    type: true,
+                    ret,
+                  });
+                })
+                .catch((e) => {
+                  return res.json({
+                    type: false,
+                    data: e.toString(),
+                  });
+                });
+        }
+        else{
+            return res.json({
+                type: false,
+                data:"not found",
+              });
+        }
+    
+    })
+    .catch((e) => {
+      return res.json({
+        type: false,
+        data: e.toString(),
+      });
+    });
+});
 
 module.exports = app;
